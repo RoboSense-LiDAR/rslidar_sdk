197863cc880085ddbfc29753311db7d4e385f7d4
ron.zheng <ron.zheng@robosense.cn>
2022-05-17 14:14:34 +0800

    feat: default pcap_repeat to true

--- src/source/source_driver.hpp
+++ src/source/source_driver.hpp
@@ -86,7 +86,7 @@ inline void SourceDriver::init(const YAML::Node& config)
   yamlRead<bool>(driver_config, "use_vlan", driver_param.input_param.use_vlan, false);
   yamlRead<std::string>(driver_config, "pcap_path", driver_param.input_param.pcap_path, "");
   yamlRead<float>(driver_config, "pcap_rate", driver_param.input_param.pcap_rate, 1);
-  yamlRead<bool>(driver_config, "pcap_repeat", driver_param.input_param.pcap_repeat, false);
+  yamlRead<bool>(driver_config, "pcap_repeat", driver_param.input_param.pcap_repeat, true);
   yamlRead<uint16_t>(driver_config, "user_layer_bytes", driver_param.input_param.user_layer_bytes, 0);
   yamlRead<uint16_t>(driver_config, "tail_layer_bytes", driver_param.input_param.tail_layer_bytes, 0);
 
