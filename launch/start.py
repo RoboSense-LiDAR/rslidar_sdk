e77ab151e9ef6927d9c5b855017c91e0abbff888
ron.zheng <ron.zheng@robosense.cn>
2022-07-25 15:06:51 +0800

    Revert "fix: fix launch file for ros2 elequent"
    This reverts commit 09d3c5ab87740a377418b79632f024f12bc4a64b.

--- launch/start.py
+++ launch/start.py
@@ -1,21 +1,12 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+
 def generate_launch_description():
+
     rviz_config=get_package_share_directory('rslidar_sdk')+'/rviz/rviz2.rviz'
+
     return LaunchDescription([
-        Node(
-            package='rslidar_sdk',
-            node_namespace='rslidar_sdk',
-            node_name='rslidar_sdk_node'
-            node_executable='rslidar_sdk_node',
-            output='screen',
-        ),
-        Node(
-            package='rviz2',
-            node_namespace='rviz2',
-            node_name='rviz2',
-            node_executable='rviz2',
-            arguments=['-d',rviz_config]
-        )
+        Node(namespace='rslidar_sdk', package='rslidar_sdk', executable='rslidar_sdk_node', output='screen'),
+        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
     ])
