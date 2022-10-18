075fce098a4ba0511c63804bbe5a2064d6bc0db7
Robin Vonk <robinvonk789@gmail.com>
2022-10-18 13:38:38 +0200

    Applied build fixes for ROS 2 Humble

--- src/msg/rs_msg/lidar_point_cloud_msg.h
+++ src/msg/rs_msg/lidar_point_cloud_msg.h
@@ -33,7 +33,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 #include <string>
 #include <array>
-#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 struct RsPointXYZIRT
 {
