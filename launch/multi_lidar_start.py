4dd8f9479bd82b14c805cfae56344f7beeec6b79
조민성 <fbcore@gmail.com>
2025-10-13 19:37:29 +0900

    feat(launch): 파라미터 파일을 인자로 받도록 launch 파일 수정

--- launch/multi_lidar_start.py
+++ launch/multi_lidar_start.py
@@ -1,20 +1,27 @@
-
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('rslidar_sdk')
-    config_path = os.path.join(pkg_share, 'config', 'multi_lidar_config.yaml')
+    default_config_path = os.path.join(pkg_share, 'config', 'multi_lidar_config.yaml')
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=default_config_path,
+            description='Path to the ROS2 parameters file to use.'
+        ),
+
         Node(
             package='rslidar_sdk',
             executable='multi_lidar_node',
             name='multi_lidar_node',
             output='screen',
-            parameters=[config_path],
+            parameters=[LaunchConfiguration('params_file')],
             emulate_tty=True,
         )
     ])
