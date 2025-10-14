c65c48cb99b09bfb263dec9df0eb60d2d8c6eef8
조민성 <fbcore@gmail.com>
2025-10-15 02:26:50 +0900

    TF 관련 수정

--- launch/multi_lidar_start.py
+++ launch/multi_lidar_start.py
@@ -23,31 +23,5 @@ def generate_launch_description():
             output='screen',
             parameters=[LaunchConfiguration('params_file')],
             emulate_tty=True,
-        ),
-
-        # lidar_0의 TF를 발행하는 노드
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_to_lidar0_tf',
-            arguments=[
-                '0.687', '-0.357', '0.709',      # x, y, z 위치 (m)
-                '1.277', '-1.317', '-4.417',     # roll, pitch, yaw 자세 (rad)
-                'base_link',                    # 부모 프레임
-                'lidar_0_link'                  # 자식 프레임
-            ]
-        ),
-
-        # lidar_1의 TF를 발행하는 노드
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_to_lidar1_tf',
-            arguments=[
-                '-0.300', '0.360', '-0.000',     # x, y, z 위치 (m)
-                '-0.006', '0.079', '-3.945',     # roll, pitch, yaw 자세 (rad)
-                'base_link',                    # 부모 프레임
-                'lidar_1_link'                  # 자식 프레임
-            ]
-        ),
+        )
     ])
